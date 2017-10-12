33a44adcebcb773010b418c3ea908bccd5339073
alexserga <snike1985@gmail.com>
2017-10-12 20:42:42 +0300

    change texts and links

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -89,6 +89,7 @@
         var _obj = obj,
             _btn = _obj.find( '.menu__btn' ),
             _item = _obj.find( '.menu__item' ),
+            _anchor = $( '.anchor' ),
             _wrap = _obj.find( '.menu__wrap' ),
             _scrollConteiner = $( 'html' );
 
@@ -106,26 +107,27 @@
                     }
                 });
 
-                _item.on({
+                _anchor.on({
                     'click': function() {
-                        // event.preventDefault();
-                        // var elem = $( this ),
-                        //     id = elem.attr( 'href' ),
-                        //     way = $( id ).offset().top - $( '.site__header' ).outerHeight() + 1,
-                        //     duration = 1000,
-                        //     scrollWrap = $( 'body, html' );
-                        //
-                        // if ( !elem.hasClass( 'active' ) ) {
-                        //     scrollWrap.animate( { scrollTop: way }, duration );
-                        //
-                        //     setTimeout( function () {
-                        //         scrollWrap.animate( { scrollTop: way - 1 }, 1 );
-                        //     }, duration );
-                        //
-                        //     _item.removeClass( 'active' );
-                        //     elem.addClass( 'active' );
-                        //     _hideMenu();
-                        // }
+                        _hideMenu();
+                        event.preventDefault();
+                        var elem = $( this ),
+                            id = elem.attr( 'href' ),
+                            way = $( id ).offset().top - $( '.site__header' ).outerHeight() + 1,
+                            duration = 1000,
+                            scrollWrap = $( 'body, html' );
+
+                        if ( !elem.hasClass( 'active' ) ) {
+                            scrollWrap.animate( { scrollTop: way }, duration );
+
+                            setTimeout( function () {
+                                scrollWrap.animate( { scrollTop: way - 1 }, 1 );
+                            }, duration );
+
+                            _item.removeClass( 'active' );
+                            elem.addClass( 'active' );
+                            _hideMenu();
+                        }
 
                     }
                 });
